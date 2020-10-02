8dcf80aba097fa757ce83ec987e1c69a734acefd
Patrícia Oliveira <poliveira@ubiwhere.com>
2020-10-02 12:59:49 +0100

    Add more run instructions

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,3 +6,11 @@ robot --outputdir ./results .
 #Override binding on runtime
 #robot --variable binding:"MQTT" --outputdir ./results_MQTT .
 #robot --variable binding:"HTTP" --outputdir ./results_HTTP .
+
+#Gather results from both runs in the same report
+#rebot ./results_HTTP/output.xml ./results_MQTT/output.xml
+
+#Generate allure report
+#robot --variable binding:"MQTT" --listener allure_robotframework .
+#allure generate . 
+#allure open allure-report  
